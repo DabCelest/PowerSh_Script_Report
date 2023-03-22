7ca42bd09386c055a2d285cd2bbccd5062a7d590
Celest1 <btec.celest1@gmail.com>
2023-03-22 12:10:23 +0100

    First Script Adding

---
+++ Readme.txt
@@ -0,0 +1,23 @@
+- Script écrit en PowerShell 
+
+- Permet à un utilisateur de : 
+	 D’afficher les différentes options sous forme d’un écran d’accueil 
+	 D’afficher les principales caractéristiques du système 
+	 D’afficher les différents processus en cours d’utilisation 
+	 D’afficher le fichier log 
+	 De copier le fichier log sur une clef USB 
+
+- L’utilisateur pourra choisir les options possibles par une entrée au clavier (1-5). 
+
+- Chaque commande est inscrite dans un fichier log.
+
+- Pour ne pas à modifier dans le script, le chemin menant au dossier "MonDossier" et fichier "monfichier.log", 
+	il est conseillé de copier le dossier contenant le script et les autres fichiers .txt sur la racine Windows(C:). 
+
+
+
+
+
+
+													Made by DabCelest
+												    ------------------
---
+++ script_Report_2.ps1
@@ -0,0 +1,117 @@
+
+# Declaration de la fonction d'affichage de l'ecran d'accueil
+function Show-MainMenu {
+    #Clear-Host
+    Get-Content C:\Script_Rap\menu_report_2.txt
+}
+# Initialisation des ressources internes necessaires 
+$folderPath = "C:\MonDossier"
+$folderExists = Test-Path $folderPath
+
+if(-not $folderExists){
+    New-Item -ItemType Directory -Path $folderPath
+    Write-ToLogFile "Creation du dossier MonDossier"
+    Write-Host "Le dossier MonDossier a ete cree"
+}
+
+$filePath = "C:\MonDossier\monfichier.log"
+$fileExists = Test-Path $filePath
+
+if(-not $fileExists){
+    New-Item -ItemType File -Path $filePath
+    Write-ToLogFile "Creation du fichier monfichier.log"
+    Write-Host "Le fichier monfichier.log a ete cree"
+}
+
+# Declaration de la fonction d'affichage des caracteristiques du systeme
+function Show-SystemInfo {
+    Clear-Host
+    Write-ToLogFile "Affichage des caracteristiques du systeme"
+    Write-Host "Caracteristiques du systeme:"
+    Write-Host "Nom de l'ordinateur: $env:COMPUTERNAME"
+    Write-Host "Version de Windows: $(Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, OSArchitecture)"
+    Write-Host "Memoire Physique totale: $(Get-CimInstance Win32_ComputerSystem | Select-Object TotalPhysicalMemory | ForEach-Object { $_.TotalPhysicalMemory / 1MB }) Mo"
+    Read-Host -Prompt "Appuyez sur Entrée pour revenir au menu principal."
+}
+
+# Declaration de la fonction d'affichage des processus en cours d'utilisation
+function Show-ProcessInfo {
+    Clear-Host
+    Write-ToLogFile "Affichage des processus en cours d'utilisation"
+    Write-Host "Processus en cours d'utilisation:"
+    Get-Process | Select-Object Id, ProcessName, CPU, PrivateMemorySize, WorkingSet | Format-Table
+    Read-Host -Prompt "Appuyez sur Entree pour revenir au menu principal."
+}
+
+# Declaration de la fonction d'écriture dans le fichier log
+function Write-ToLogFile ($message) {
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $command = $($MyInvocation.Line)
+    $logEntry = "$timestamp - $command - $message"
+    Add-Content -Path $filePath -Value $logEntry
+}
+
+# Declaration de la fonction d'affichage du fichier log
+function Show-LogFile {
+    Clear-Host
+    Write-ToLogFile "Affichage du fichier log"
+    Write-Host "Contenu du fichier log:"
+    Get-Content -Path "C:\MonDossier\monfichier.log"
+    Read-Host -Prompt "Appuyez sur Entree pour revenir au menu principal."
+}
+
+# Declaration de la fonction de copie du fichier log sur une clef USB
+function Copy-LogFileToUsb {
+    Clear-Host
+    Write-ToLogFile "Tentative de copie du fichier log sur une clef USB"
+    $usbDrive = Get-WmiObject Win32_Volume | Where-Object { $_.DriveType -eq 2 } | Select-Object -First 1
+    if ($usbDrive) {
+        $usbPath = "$($usbDrive.DriveLetter):\MonDossier\monfichier.log"
+        Copy-Item -Path "C:\MonDossier\monfichier.log" -Destination $usbPath
+        Write-ToLogFile "Copie du fichier log sur une clef USB"
+        Write-Host "Le fichier log a ete copie avec succes sur la clef USB."
+    }
+    else {
+        Write-ToLogFile "tentative echouee de copie du fichier log sur une clef USB"
+        Write-Host "Aucune clef USB detectee."
+    }
+    Read-Host -Prompt "Appuyez sur Entree pour revenir au menu principal."
+}
+
+# Declaration de la fonction de sortie du programme
+function Exiting-Program {
+    exit
+}
+
+# Boucle principale du script
+$choice = 0
+while ($true) {
+    Show-MainMenu
+    $choice = Read-Host -Prompt "Entrer votre choix (1-5)."
+    switch($choice) {
+        1 {
+            Show-SystemInfo
+        }
+        2 {
+            Show-ProcessInfo
+        }
+        3 {
+            Show-LogFile
+        }
+        4 {
+            Copy-LogFileToUsb
+        }
+        5 {
+            Clear-Host
+            Write-ToLogFile "Quitter program"
+            Write-Host "SORTIE DU PROGRAMME....  AU REVOIR !"
+            Start-Sleep -Seconds 5
+            Exiting-Program
+        }
+        default {
+            Clear-Host
+            Write-ToLogFile "Choix Invalide"
+            Write-Host "Choix Invalide"
+        }
+    }
+}
